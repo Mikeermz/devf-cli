579403fc56eb46a8822d000687e0d1df78ddcaee
LigorioSalgado <salgadolg5@hotmail.com>
2018-08-14 18:46:22 -0500

    mas cambios

--- utils/sendResults.js
+++ utils/sendResults.js
@@ -2,7 +2,7 @@
  var fs = require('fs');
  var {EXERCISE_PATH,API_URL} = require('../const')
  var axios = require('axios');
- 
+ var https =  require('https');
 
 
 
@@ -11,6 +11,8 @@ module.exports = (equal) =>  {
     var current = JSON.parse(fs.readFileSync(path.join(EXERCISE_PATH,'current.json'), 'utf8'));
     var completed = JSON.parse(fs.readFileSync(path.join(EXERCISE_PATH,'completed.json'), 'utf8'));
     var {email} =JSON.parse(fs.readFileSync(path.join(EXERCISE_PATH,'.devf'), 'utf8'));
+
+
 
     if(completed.indexOf(current) == -1 && equal){
         let data = {
@@ -22,6 +24,7 @@ module.exports = (equal) =>  {
         }
         console.log(data)
         console.log(API_URL+`/student/${email}`)
+        
         axios({
             method: 'patch',
             url: API_URL+`/student/${email}`,
